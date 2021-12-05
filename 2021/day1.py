4c6b8e956838b53206a11fee87b9dfa7096fc431
Laura Salas <laurasalas96@live.com>
2021-12-04 19:20:48 -0500

    Add files via upload

---
+++ 2021/day1.py
@@ -0,0 +1,21 @@
+lines_unparsed = """input_here"""
+lines_unparsed = lines_unparsed.split("\n")
+nums = []
+for line in lines_unparsed:
+    line.replace(" ", "")
+    if line:
+        nums.append(int(line))
+
+increases = 0
+window_A = 2
+window_B = 3
+
+while window_B < len(nums):
+    sum_a = nums[window_A - 1] + nums[window_A - 1] + nums[window_A]
+    sum_b = nums[window_B - 2] + nums[window_B - 1] + nums[window_B]
+    if sum_b > sum_a:
+        increases += 1
+    window_A += 1
+    window_B += 1
+print(increases)
+
